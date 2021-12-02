081600f2317e461742b4f773a877eb103950ab64
Suzana Bogojevic <suzana.bogojevic@codecentric.de>
2021-12-02 10:44:53 +0100

    Remove testTarget and unused dependencies.

--- Package.swift
+++ Package.swift
@@ -18,32 +18,17 @@ let package = Package(
     platforms: [.iOS(.v9), .macOS(.v10_11), .tvOS(.v9), .watchOS(.v2)],
     products: [.library(name: "Auth0", targets: ["Auth0"])],
     dependencies: [
-        .package(name: "SimpleKeychain", url: "https://github.com/auth0/SimpleKeychain.git", .upToNextMajor(from: "0.12.0")),
-        .package(name: "JWTDecode", url: "https://github.com/auth0/JWTDecode.swift.git", .upToNextMajor(from: "2.5.0")),
-        .package(name: "Quick", url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "4.0.0")),
-        .package(name: "Nimble", url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "9.0.0")),
-        .package(name: "OHHTTPStubs", url: "https://github.com/AliSoftware/OHHTTPStubs.git", .upToNextMajor(from: "9.0.0"))
+        .package(name: "SimpleKeychain", url: "https://github.com/auth0/SimpleKeychain.git", .upToNextMajor(from: "0.12.5")),
+        .package(name: "JWTDecode", url: "https://github.com/auth0/JWTDecode.swift.git", .upToNextMajor(from: "2.6.3"))
     ],
     targets: [
         .target(
-            name: "Auth0", 
-            dependencies: ["SimpleKeychain", "JWTDecode", "Auth0ObjectiveC"], 
+            name: "Auth0",
+            dependencies: ["SimpleKeychain", "JWTDecode", "Auth0ObjectiveC"],
             path: "Auth0",
             exclude: ["ObjectiveC"],
             cSettings: cSettings,
             swiftSettings: swiftSettings),
-        .target(name: "Auth0ObjectiveC", path: "Auth0/ObjectiveC", cSettings: cSettings),
-        .testTarget(
-            name: "Auth0Tests",
-            dependencies: [
-                "Auth0", 
-                "Quick", 
-                "Nimble", 
-                .product(name: "OHHTTPStubsSwift", package: "OHHTTPStubs")
-            ],
-            path: "Auth0Tests",
-            exclude: ["ObjectiveC"],
-            cSettings: cSettings,
-            swiftSettings: swiftSettings)
+        .target(name: "Auth0ObjectiveC", path: "Auth0/ObjectiveC", cSettings: cSettings)
     ]
 )
